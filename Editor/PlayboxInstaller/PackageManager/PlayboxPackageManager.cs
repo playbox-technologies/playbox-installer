a150526e1b199c82213d8317af90e1f173333986
Sergiychuk Viachaslau <qertysig@gmail.com>
2026-01-14 12:24:40 +0300

    Add playbox package manager

---
+++ Editor/PlayboxInstaller/PackageManager/PlayboxPackageManager.cs
@@ -0,0 +1,13 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+
+namespace Editor.PlayboxInstaller.PackageManager
+{
+    public class PlayboxPackageManager : EditorWindow
+    {
+        
+    }
+}
+
+#endif
--- Editor/PlayboxInstaller/Stages/StageWindowContext.cs
+++ Editor/PlayboxInstaller/Stages/StageWindowContext.cs
@@ -1,6 +1,6 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
 
-#if UNITY_EDITOR
+using UnityEditor;
 
 namespace PlayboxInstaller
 {
